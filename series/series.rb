0c55d3e765375ae6352a2b663a0606c1d8b58632
Ellen Keal <ellenkeal@gmail.com>
2018-09-28 09:19:37 -0500

    fixed tabbing and downloaded exercise

--- series/series.rb
+++ series/series.rb
@@ -1,13 +1,14 @@
 class Series
-    def slices(num)
-        if num > @num.length then throw ArgumentError end
-        array = []
-        for i in 0..(@num.length-num)
-            array << @num[i..(i+num-1)]
-        end
-        return array
+  def slices(num)
+    if num > @num.length then throw ArgumentError end
+      array = []
+      for i in 0..(@num.length-num)
+        array << @num[i..(i+num-1)]
+      end
+      return array
     end
-    def initialize(str)
-        @num = str
-    end
+  end
+  def initialize(str)
+    @num = str
+  end
 end
--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -1,23 +1,23 @@
 class Phrase
-    def initialize(phrase)
-        @phrase = phrase.split(/[^a-zA-Z0-9']/)
-    end
-    def word_count
-        words = {}
-        for word in @phrase
-            if word.length > 0
-                word.downcase!
-                word.chomp!("'")
-                word.reverse!
-                word.chomp!("'")
-                word.reverse!
-                if (!words[word] && word.length > 0)
-                    words[word] = 1
-                elsif word.length > 0
-                    words[word] += 1
-                end
-            end
+  def initialize(phrase)
+    @phrase = phrase.split(/[^a-zA-Z0-9']/)
+  end
+  def word_count
+    words = {}
+    for word in @phrase
+      if word.length > 0
+        word.downcase!
+        word.chomp!("'")
+        word.reverse!
+        word.chomp!("'")
+        word.reverse!
+        if (!words[word] && word.length > 0)
+          words[word] = 1
+        elsif word.length > 0
+          words[word] += 1
         end
-        return words
+      end
     end
+    return words
+  end
 end
