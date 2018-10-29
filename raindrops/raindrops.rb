ae7e62c27bc27e64f0d3390d0283721fb616bf01
Ellen Keal <ellenkeal@gmail.com>
2018-10-29 11:21:04 -0500

    final raindrops

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,12 +1,13 @@
 class Raindrops
-  def self.convert(num)
-    rain_sounds = {
-      3 => 'Pling',
-      5 => 'Plang',
-      7 => 'Plong',
-    }
+  RAIN_SOUNDS = {
+    3 => 'Pling',
+    5 => 'Plang',
+    7 => 'Plong',
+  }
 
-    applicable_rain_sounds = rain_sounds.select { |key, val| num%key == 0 }
-    applicable_rain_sounds.empty? ? num.to_s : applicable_rain_sounds.values.join
+  def self.convert(number)
+
+    sounds = RAIN_SOUNDS.select { |key, val| number%key == 0 }.values
+    sounds.empty? ? number.to_s : sounds.join
   end
 end
