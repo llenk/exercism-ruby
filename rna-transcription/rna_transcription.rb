ea3db8627dbaca6d48fc8b0e0cbd8ba5af0c1c4d
Ellen Keal <ellenkeal@gmail.com>
2018-10-30 14:30:51 -0500

    rna fixes

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,18 +1,5 @@
 class Complement
-    def self.of_dna(dna)
-        rna = ''
-        for i in 0..(dna.length - 1)
-            case dna[i]
-            when 'G'
-                rna << 'C'
-            when 'C'
-                rna << 'G'
-            when 'T'
-                rna << 'A'
-            when 'A'
-                rna << 'U'
-            end
-        end
-        return rna
-    end
+  def self.of_dna(dna)
+    dna.tr('ACGT', 'UGCA')
+  end
 end
