e7d1fcb6b3370a5c8d441af10493a3e43ff2ad07
Ellen Keal <ellenkeal@gmail.com>
2018-10-31 14:08:30 -0500

    update syntax

--- collatz-conjecture/collatz_conjecture.rb
+++ collatz-conjecture/collatz_conjecture.rb
@@ -1,6 +1,6 @@
 class CollatzConjecture
     def self.steps(num)
-        if num <= 0 then raise ArgumentError end
+        raise ArgumentError if num <= 0
         num == 1 ? 0 : num.even? ? self.steps(num/2) + 1 : self.steps(num * 3 + 1) + 1
     end
 end
