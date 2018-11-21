25c4bfad56e0c3ee57d020a5f8aab840d662d8ad
Ellen Keal <ellenkeal@gmail.com>
2018-11-21 10:52:13 -0600

    updates on matrix problem

--- matrix/matrix.rb
+++ matrix/matrix.rb
@@ -1,12 +1,18 @@
 class Matrix
   def initialize(string_representation)
     raise ArgumentError if string_representation.empty?
-    @matrix = string_representation.split("\n").map{ |row| row.split.map(&:to_i) }
+    @matrix = string_representation.lines.map{ |row| row.split.map(&:to_i) }
   end
+
   def rows
     @matrix
   end
+
   def columns
-    @matrix[0].map.with_index{ |_x, index| @matrix.map{ |row| row[index] } }
+    self
+  end
+
+  def [](index)
+    @matrix.map{ |row| row[index] }
   end
 end
