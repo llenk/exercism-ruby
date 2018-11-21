031711b91fbfd11f8e8e70a6a709e8212a76038e
Ellen Keal <ellenkeal@gmail.com>
2018-11-21 10:59:49 -0600

    responded to feedback on collatz conjecture

--- collatz-conjecture/collatz_conjecture.rb
+++ collatz-conjecture/collatz_conjecture.rb
@@ -1,6 +1,9 @@
 class CollatzConjecture
-    def self.steps(num)
-        raise ArgumentError if num <= 0
-        num == 1 ? 0 : num.even? ? self.steps(num/2) + 1 : self.steps(num * 3 + 1) + 1
-    end
+  def self.steps(num)
+    raise ArgumentError if num <= 0
+
+    return 0 if num == 1
+
+    num.even? ? self.steps(num/2) + 1 : self.steps(num * 3 + 1) + 1
+  end
 end
