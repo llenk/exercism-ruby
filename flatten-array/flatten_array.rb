52f34d8a39a48f1854a65329e543e0bdb2b2ee34
Ellen Keal <ellenkeal@gmail.com>
2019-02-26 14:30:35 -0600

    flatten array solution

--- flatten-array/flatten_array.rb
+++ flatten-array/flatten_array.rb
@@ -1,5 +1,13 @@
 class FlattenArray
   def self.flatten(array)
+    remove_nils(flatten_recursive(array))
+  end
 
+  def self.flatten_recursive(array)
+    array.map { |element| element.is_a?(Array) ? flatten_recursive(element) : element }.flatten
+  end
+
+  def self.remove_nils(array)
+    array.select { |element| element }
   end
 end
