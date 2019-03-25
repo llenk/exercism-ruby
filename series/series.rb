c8e0c0cd4c054f3770c0754d67bb4f38bd7ab6bc
Ellen Keal <ellenkeal@gmail.com>
2019-03-25 09:23:43 -0500

    new solution to series

--- series/series.rb
+++ series/series.rb
@@ -1,14 +1,16 @@
 class Series
-  def slices(num)
-    if num > @num.length then throw ArgumentError end
-      array = []
-      for i in 0..(@num.length-num)
-        array << @num[i..(i+num-1)]
-      end
-      return array
-    end
-  end
   def initialize(str)
     @num = str
+  end
+
+  def slices(num)
+    raise ArgumentError if num > @num.length
+    original_string_array = @num.split(//)
+    substring_array = []
+
+    original_string_array.slice(0, @num.length - num + 1).each_index do |index|
+      substring_array << @num.slice(index..(index + num - 1))
+    end
+    substring_array
   end
 end
